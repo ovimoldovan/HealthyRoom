a81262a8b640a1a200c6badbe17ed8e124775465
alinvoicu26 <44782566+alinvoicu26@users.noreply.github.com>
2020-06-14 11:38:17 +0300

    Add files via upload

---
+++ sloeber.ino.cpp
@@ -0,0 +1,23 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2019-12-17 17:32:01
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "dht.h"
+#include "LiquidCrystal.h"
+#include <Wire.h>
+
+void setup() ;
+void recvWithEndMarker() ;
+void showNewData() ;
+void motor_onoff() ;
+void people_counter() ;
+void loop() ;
+
+#include "MMIproject.ino"
+
+
+#endif
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ C:/Users/alinvoicu26/eclipse-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
